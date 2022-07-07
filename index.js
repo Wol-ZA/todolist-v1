da0b8eb1fa26f14d2cc5a547136976c94524bd95
Wol-ZA <johannesgvdm@gmail.com>
2022-07-07 21:28:23 +0200

    Changed the toDo List to function with a Mongo database to add and remove items without server restart

--- index.js
+++ index.js
@@ -2,33 +2,67 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const date = require(__dirname + "/date.js");
-const items = [];
-const workItems = [];
+const mongoose = require("mongoose");
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:27017/todoslistDB');
+const itemsSchema = {
+  name: String
+};
+const Item = mongoose.model('Item', itemsSchema);
+
+const Item1 = new Item({
+  name: 'Welcome to your To Do List!!'
+});
+const Item2 = new Item({
+  name: 'Hit the + button to add new items'
+});
+const Item3 = new Item({
+  name: '<--- Hit this to delete an item'
+});
+const defaultItems = [Item1,Item2,Item3];
 
 app.get("/", (req,res) =>{
   const day = date.getDate();
-  res.render("list", {listTitle: day, newListItems: items});
+  Item.find((err,items)=>{
+    if(items.length === 0){
+      Item.insertMany(defaultItems, (err) =>{
+        if (err){
+          console.log(err);
+        }else{
+          console.log("Item added successfully");
+        }
+        res.redirect("/");
+      });
+    }else{
+        res.render("list", {listTitle: day, newListItems: items});
+    }
+  });
+
 });
 
 app.post("/" , (req,res) => {
-    const item = req.body.newItem;
-    if (req.body.list === "Work") {
-      workItems.push(item);
-      res.redirect("/work");
-    }else{
-      if (item != '') {
-        items.push(item);
-        res.redirect("/");
-      }else{
-        res.redirect("/");
-      }
-    }
-
+  const itemName = req.body.newItem
+  const item = new Item({
+    name: itemName
+  });
+  item.save();
+  res.redirect("/");
 });
 
+app.post("/delete" , (req,res) =>{
+  const checkedItem = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItem,(err)=>{
+    if (err){
+      console.log(err);
+    }else{
+      console.log("Item removed successfully");
+    }
+    res.redirect("/");
+  });
+});
 
 app.get("/work", (req,res) => {
   res.render("list",{listTitle: "Work List", newListItems: workItems})
